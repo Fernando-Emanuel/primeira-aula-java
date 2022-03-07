fc0a6ef433bdd3e1cc5509aad487638f8e93cec6
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-03-07 16:41:43 +0000

    Main

--- Ola.java
+++ Ola.java
@@ -1,9 +1,23 @@
 public class Ola {
-    private static int soma(int a, int b) {
+    public static int soma(int a, int b) {
         return a + b;
     }
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!!!");
-        System.out.println(soma(3, 5));
-    }
-} 
+        System.out.println("Olá Mundo!!");
+        System.out.println(soma(4,6));
+
+        boolean bol = false;
+        char ch = 'a';
+        byte intB = 2;
+        short intS = 200;
+        int intN = 1000;
+        long intL = 2000;
+        float fNum = 2.5f;
+        double dNum = 2.5;
+    
+
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+
+    } 
+}
